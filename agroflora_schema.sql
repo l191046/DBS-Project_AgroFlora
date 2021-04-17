19095e7e5b0db85ddb8b785d041a35f438ef489b
l191046 <82677562+l191046@users.noreply.github.com>
2021-04-18 00:14:18 +0500

    Schema tweaked, added data

--- agroflora_schema.sql
+++ agroflora_schema.sql
@@ -3,8 +3,18 @@ DROP DATABASE PRJT
 CREATE DATABASE PRJT
 USE PRJT
 
+DROP TABLE [Admin]
+DROP TABLE Customer
+DROP TABLE Retailer
+DROP TABLE Product
+DROP TABLE Category
+DROP TABLE Rating
+DROP TABLE Purchase
+DROP TABLE PaymentType
+
+
 CREATE TABLE [Admin] (
-	[Login]		varchar(20) NOT NULL,
+	[UserName]	varchar(20) NOT NULL,
 	[Password]	varchar(20) NOT NULL,
 
 	AdminID		int NOT NULL,
@@ -15,31 +25,30 @@ CREATE TABLE [Admin] (
 );
 
 CREATE TABLE Customer (
-	[Login]		varchar(20) NOT NULL,
+	[UserName]		varchar(20) NOT NULL,
 	[Password]	varchar(20) NOT NULL,
 
 	CustomerID	int NOT NULL,
 	Fname		varchar(20) NOT NULL,
 	Lname		varchar(20) NOT NULL,
 	Email		varchar(30),
-	[Address]	varchar(30),
+	[Address]	varchar(50),
 	DOB			date,
 	Points		int,
-	Contact		int,
+	Contact		char(11),
 	CreditCard	char(16)
 );
 
 CREATE TABLE Retailer (
-	[Login]			varchar(20) NOT NULL,
+	[UserName]		varchar(20) NOT NULL,
 	[Password]		varchar(20) NOT NULL,
 
 	RetailerID		int NOT NULL,
-	Fname			varchar(20) NOT NULL,
-	Lname			varchar(20) NOT NULL,
+	[Name]			varchar(50) NOT NULL,
 	Email			varchar(30),
 	[Address]		varchar(30),
-	NTN				int NOT NULL,
-	Contact			int,
+	NTN				char(13),
+	Contact			char(11),
 	BankAccount		varchar(20) NOT NULL
 );
 
@@ -50,7 +59,7 @@ CREATE TABLE Product (
 	Price			float NOT NULL,
 	CategoryID		int DEFAULT 0,
 	Stock			int,
-	[Description]	varchar(100),
+	[Description]	varchar(300),
 	DateAdded		date NOT NULL,
 	[Status]		bit
 );
@@ -72,7 +81,6 @@ CREATE TABLE Purchase (
 	CustomerID	int NOT NULL,
 	ProductID	int NOT NULL,
 	Quantity	int NOT NULL,
-	Price		int NOT NULL,
 	[Date]		datetime NOT NULL,
 	PaymentType	int
 );
@@ -81,6 +89,7 @@ CREATE TABLE PaymentType (
 	[PaymentTypeID]	int NOT NULL,
 	[Name]			varchar(25)
 );
+
 
 --PRIMARY KEYS
 ALTER TABLE [Admin] 
@@ -144,4 +153,113 @@ ALTER TABLE Product ADD CONSTRAINT CHK_Price
 CHECK (Price >= 0);
 
 
+--DATA
+Insert Into [Admin] ([UserName],[Password],AdminID,Fname,Lname,Email,CNIC) values
+('sukhanamir','cruchyrock478',1,'Sukhan','Amir','sukhanamir23@gmail,com','3520245930124'),
+('abdulmuneem','poniacbandit39',2,'Rana','Abdul Muneem','abdulmuneem76@gmail.com','3520258491028'),
+('raziahmed', 'inferno889', 3,'Razi','Ahmed','raziahmed98@gmail.com','3520285940314');
 
+Insert Into [Customer] ([UserName],[Password],CustomerID,Fname,Lname,Email,[Address],DOB,Points,Contact,CreditCard) values
+('abdulsami','rollarcoaster45',4,'Abdul Sami','Butt','abdulsami@outlook.com','378 G3 Johar Town,Lahore ','1978-03-21',0,'03594830126','5940302768943670'),
+('hajrazubair','notsoweird23',6,'Hajra','Zubair','hzubair44@gmail.com','44 block A DHA,Karachi ','1999-03-12',0,'03657483296','5746398076571297'),
+('ridaahmed','iamoutofpasswords33',8,'Rida','Ahmed','ridaahmed5@gmail.com','984 J1 Wapda Town,Lahore ','2002-12-5',0,'03970543619','9715489065468491'),
+('mohsinkhan','tranquialitybase',10,'Mohsin','Khan','mohsinkhan@yahoo.com','44 street 55 Peshawar Cantt,Peshawar ','2001-06-6',0,'0346372895','8493016574839265'),
+('RaziAhmad722', 'lalalalala', 1,'Razi','Ahmad', 'razi722@gmail.com' , '122 Cornelia Street', '1998-01-21', 0, '03325215251', '0123456789101112'),
+('MunimMuneem', 'Monum', 2,'R','anaAbdulMuneem', 'monum1252@gmail.com' , 'chakk no. 543', '2001-01-21', 0, '03325885251', '0123411189101112'),
+('Sukhan123', 'Perplexedhooman', 3,'Su','Khan', 'Sukhan123@gmail.com' , 'F2 block lacas', '2000-01-21', 0, '03366215251', '0123422289101112'),
+('AAR123', '12344n', 5,'Ahsan','Abdurrehman', 'aar123@gmail.com' , '123 G block Bahria', '2000-01-31', 0, '03376215251', '0129422289101112'),
+('zainRafique21', '19344n', 7,'Zain','Rafique', 'zain123@gmail.com' , '142 F block Valencia Town', '2000-02-23', 0, '03176215251', '0169422289101112'),
+('sherry123', '19944n', 9,'Shaheer','Akhtar', 'sherry0123@gmail.com' , '142 F block Johar Town', '2000-05-30', 0, '03176015251', '0169422280001112');
+
+INSERT INTO Retailer VALUES
+('Flowershop', '01234567', 1, 'Flowershop', 'flowershop@gmail.com', 'Wapda Town Lahore', '0123456', '03361111111', '12345678910111213'),
+('ParadiseGarden', '12345678', 2, 'The Paradise Garden', 'paradisegarden@gmail.com', 'Bahria Town Karachi', '1234567', '03231212121', '12345678910111214'),
+('GTerrace', '23456789', 3, 'Green Terrace', 'greenterrace@gmail.com', 'DHA Phase3 Lahore', '2345678', '03341112222', '12345678910111215'), 
+('GardenDel', '34567890', 4, 'Garden Delights', 'gardendelights@gmail.com', 'Model Town Lahore', '3456789', '03329998888', '12345678910111216'),
+('PKfertilizer', '45678901', 5, 'PK Fertilizers', 'PKfertilizers@gmail.com', 'F2 Islamabad', '4567890', '03235555544', '12345678910111217');
+
+INSERT INTO Category VALUES
+(1, 'Plant'),
+(2, 'Seeds'),
+(3, 'Fertilizer'),
+(4, 'Pot'),
+(5, 'Tool'),
+(6, 'Other');
+
+INSERT INTO Product VALUES
+(1, 1, 'Petunia', 50, 1, 100, 'Petunia flower with pot grown locally', '2021-1-17', 1),
+(2, 1, 'Terminaria', 1200, 1, 50, 'Terminaria plant grown locally', '2021-2-18', 1),
+(3, 1, 'ShoeFlower', 350, 1, 80, 'Shoeflower grown locally', '2021-2-25', 1),
+(4, 1, 'Ixora', 1100, 1, 40, 'Ixora flower imported', '2021-3-23', 1),
+(5, 2, 'Lemon Grass', 200, 1, 50, 'Healthy bush of lemon grass plant', '2021-4-10', 1),
+(6, 2, 'Euphorbia', 450, 1, 90, 'Fully grown Euphorbia flower', '2021-4-11', 1),
+(7, 2, 'Avocaria', 4750, 1, 30, 'Fully grown Avocaria tree grown locally', '2021-4-13', 1),
+(8, 2, 'Rose', 80, 1, 150, 'Rose flower grown locally', '2021-4-23', 1),
+(9,3,'Urea Fertilizer',12000,3,600,'Comes in 50KG bags','2021-01-01',1),
+(10,3,'Red Pots',300,4,200,'Hangable','2020-05-15',1),
+(11,3,'Tomato Seeds',100,2,700,'Fast Growing','2020-07-16',1),
+(12,3,'Tulip Seeds',700,2,800,'All Season','2021-01-07',1),
+(13,4,'Money Plant',50,1,400,'Indoor Plant','2020-09-22',1),
+(14,4,'Spring Onion Seed',200,2,1200,'Kitchen Garden Item','2021-02-19',1),
+(15,4,'Artifical Grass',700,6,400,'Comes in patches of 30 yards','2021-04-01',1),
+(16,4,'Shovel',200,5,300,'Tool for digging','2020-04-02',1),
+(17, 5, 'Mattock', 1000, 5, 100, 'hand tool used for digging, prying, and chopping', '2021-1-01', 1),
+(18, 5, 'NPK Fertilizer', 8000, 3, 50, '10kg NPK Fully organic fertilizers', '2021-1-01', 1),
+(19, 5, 'Calcium Fertilizer', 6000, 3, 60, '10kg Calcium rich fertilizers', '2021-1-01', 1),
+(20, 5, 'Rodenticide', 2000, 5, 20, 'Kill pesky rodents by giving them this poison. 1kg Bag.', '2021-1-01', 1);
+
+INSERT INTO PaymentType VALUES
+(1,'Credit Card'),
+(2,'Cash on delivery'),
+(3,'Cheque on delivery'),
+(4,'Debit Card'),
+(5,'Mobile Payment');
+
+INSERT INTO Purchase VALUES
+(1, 4, 2, 10, '2021-2-24', 1),
+(2, 4, 4, 5, '2021-2-24', 1),
+(3, 6, 9, 1, '2021-2-10', 2),
+(4, 6, 11, 5, '2021-2-10', 2),
+(5, 6, 14, 3, '2021-4-20', 2),
+(6, 8, 10, 15, '2021-06-15', 5),
+(7, 10, 15, 1, '2021-02-01', 1),
+(8, 10, 17, 1, '2021-02-01', 1),
+(9, 1, 6, 3, '2021-4-15', 2),
+(10, 1, 13, 5, '2021-4-20', 2),
+(11, 1, 1, 2,'2021-10-2',1),
+(12, 1, 1, 4,'2021-9-2',2),
+(13, 2, 3, 1,'2022-8-2',3),
+(14, 2, 2, 7,'2022-7-3',1),
+(15, 3, 4, 5,'2021-12-21',5),
+(16, 5, 7, 1,'2021-12-11',4),
+(17, 5, 12, 3,'2021-12-12',3),
+(18, 5, 14, 2,'2021-12-25',1),
+(19, 7, 16, 1,'2021-12-14',1),
+(20, 7, 19, 2,'2021-12-16',5);
+
+INSERT INTO Rating VALUES
+(17, 2, 1, 'Not Durable'),
+(20, 5, 4, 'Excellent Results'),
+(14, 2, 3, 'Satisfactory'),
+(14, 7, 5, NULL),
+(16, 1, 4, 'A Must Have'),
+(12, 4, 5, 'Extremely helpful for my flowershop'),
+(20, 2, 4, NULL),
+(9, 1, 4, 'Boosted the growth of my plants'),
+(18, 1, 3, NULL),
+(1, 5, 4, 'One of the most beautiful plants in my garden'),
+(8, 5, 3,' The Growth went down in winters'),
+(19, 2, 4, NULL),
+(5, 3, 5, 'Surely left a happy customer'),
+(12, 1, 3, NULL),
+(10, 2, 3, 'They are pretty cute')
+
+
+SELECT * FROM Admin
+SELECT * FROM Customer
+SELECT * FROM Retailer
+SELECT * FROM Product
+SELECT * FROM Category
+SELECT * FROM Rating
+SELECT * FROM PaymentType
+SELECT * FROM Purchase
