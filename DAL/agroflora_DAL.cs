d9c6548f8100b0bd90d4dc0ef25fd907ccb1bc24
Sukhan Amir <82700639+SukhanAmir@users.noreply.github.com>
2021-05-24 19:07:02 +0500

    Add files via upload

--- DAL/agroflora_DAL.cs
+++ DAL/agroflora_DAL.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.Web.Management;
 
 namespace Agroflora.DAL
 {
@@ -120,7 +122,7 @@ namespace Agroflora.DAL
 				//get output
 				found = Convert.ToInt32(command.Parameters["@found"].Value);
 
-				if(found == 1)
+				if (found == 1)
 				{
 					using (SqlDataAdapter da = new SqlDataAdapter(command))
 					{
@@ -141,7 +143,7 @@ namespace Agroflora.DAL
 
 			return found;
 		}
-	
+
 		public int retailer_signin(String username, String password, ref DataTable DT)
 		{
 			int found = 0;
@@ -185,6 +187,110 @@ namespace Agroflora.DAL
 			}
 
 			return found;
+		}
+		public int customer_signin(String username, String password, ref DataTable DT)
+		{
+			int found = 0;
+			DataSet resultSet = new DataSet();
+			SqlConnection con = new SqlConnection(connectionString);
+			con.Open();
+			SqlCommand command;
+
+			try
+			{
+				command = new SqlCommand("customer_signin", con);
+				command.CommandType = CommandType.StoredProcedure;
+				//establish parameters
+				command.Parameters.Add("@username", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@password", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@found", SqlDbType.Int).Direction = ParameterDirection.Output;
+				//set parameters
+				command.Parameters["@username"].Value = username;
+				command.Parameters["@password"].Value = password;
+
+				command.ExecuteNonQuery();
+				//get output
+				found = Convert.ToInt32(command.Parameters["@found"].Value);
+
+				if (found == 1)
+				{
+					using (SqlDataAdapter da = new SqlDataAdapter(command))
+					{
+						da.Fill(resultSet);
+					}
+					DT = resultSet.Tables[0];
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("SQL Error: " + ex.Message.ToString());
+			}
+			finally
+			{
+				con.Close();
+			}
+
+			return found;
+		}
+		public int customer_signup(string fname, string lname, string username, string email, string password, string address, string dob, ref DataTable DT)
+		{
+			int success = 0;
+			int alreadyexists = 0;
+			DataSet resultSet = new DataSet();
+			SqlConnection con = new SqlConnection(connectionString);
+			con.Open();
+			SqlCommand command;
+			try
+			{
+				command = new SqlCommand("customer_signup", con);
+				command.CommandType = CommandType.StoredProcedure;
+				//establish parameters
+				command.Parameters.Add("@fname", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@lname", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@username", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@email", SqlDbType.VarChar, 30);
+				command.Parameters.Add("@password", SqlDbType.VarChar, 20);
+				command.Parameters.Add("@address", SqlDbType.VarChar, 50);
+				command.Parameters.Add("@dob", SqlDbType.Date);
+				command.Parameters.Add("@alreadyexists", SqlDbType.Int).Direction = ParameterDirection.Output;
+				//set parameters
+				command.Parameters["@fname"].Value = fname;
+				command.Parameters["@lname"].Value = lname;
+				command.Parameters["@username"].Value = username;
+				command.Parameters["@email"].Value = email;
+				command.Parameters["@password"].Value = password;
+				command.Parameters["@address"].Value = address;
+				command.Parameters["@dob"].Value = dob;
+				command.ExecuteNonQuery();
+				//get output
+				alreadyexists = Convert.ToInt32(command.Parameters["@alreadyexists"].Value);
+				int shortpassword = 0;
+				int length = password.Length;
+				if(length<8)
+				{
+					shortpassword = 1;
+				}
+
+				if ((alreadyexists == 0) && (shortpassword==0 ))
+				{
+					using (SqlDataAdapter da = new SqlDataAdapter(command))
+					{
+						da.Fill(resultSet);
+					}
+					DT = resultSet.Tables[0];
+					success = 1;
+				}
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine("SQL Error: " + ex.Message.ToString());
+			}
+			finally
+			{
+				con.Close();
+			}
+
+			return success;
 		}
 	}
 }
